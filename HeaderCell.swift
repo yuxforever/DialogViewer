9ef7b7757b4f0737382b6340b17b3c246c642ba4
Yu Ma <michaelma@gwmail.gwu.edu>
2016-10-15 14:39:35 -0400

    1
    1

---
+++ HeaderCell.swift
@@ -0,0 +1,29 @@
+//
+//  HeaderCell.swift
+//  DialogViewer
+//
+//  Created by Yu Ma on 4/15/16.
+//  Copyright © 2016 Yu Ma. All rights reserved.
+//
+
+import UIKit
+
+class HeaderCell: ContentCell {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        label.backgroundColor = UIColor(red: 0.9, green: 0.9,
+            blue: 0.8, alpha: 1.0)
+        label.textColor = UIColor.blackColor()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    override class func defaultFont() -> UIFont {
+        return UIFont.preferredFontForTextStyle(UIFontTextStyleHeadline)
+    }
+
+
+}
